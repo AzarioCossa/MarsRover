5733357836aa6064cf8839634a9c910cad9fd575
AzarioCossa <cossaazario@gmail.com>
2024-11-18 15:54:06 +0100

    add and implement Direction, Position and Rover

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -1,3 +1,12 @@
 public class Rover {
-
+	private Position position;
+	private Direction DIRECTION;
+	
+	public Rover(Position position, Direction dIRECTION) {
+		super();
+		this.position = position;
+		DIRECTION = dIRECTION;
+	}
+	
+	
 }
