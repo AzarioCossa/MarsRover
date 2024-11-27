2405756d919a90d067bcc670d7cc6e205b8c13d8
Ilyas Boukhari <ilyasboukh87@gmail.com>
2024-11-27 08:57:33 +0100

    add RoverTest class

--- src/test/java/RoverTest.java
+++ src/test/java/RoverTest.java
@@ -5,8 +5,51 @@ import org.junit.jupiter.api.Test;
 class RoverTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    public void testMoveForward() {
+        Rover rover = new Rover();
+        MoveRover moveRover = new MoveRover(rover);
 
+        moveRover.move("f");
+        assertEquals(0, rover.getPosition().getX());
+        assertEquals(1, rover.getPosition().getY());
+    }
+
+    @Test
+    public void testMoveBackwards() {
+        Rover rover = new Rover();
+        MoveRover moveRover = new MoveRover(rover);
+
+        moveRover.move("b");
+        assertEquals(0, rover.getPosition().getX());
+        assertEquals(-1, rover.getPosition().getY());
+    }
+
+    @Test
+    public void testTurnLeft() {
+        Rover rover = new Rover();
+        MoveRover moveRover = new MoveRover(rover);
+
+        moveRover.move("l");
+        assertEquals(Direction.WEST, rover.getDirection());
+    }
+
+    @Test
+    public void testTurnRight() {
+        Rover rover = new Rover();
+        MoveRover moveRover = new MoveRover(rover);
+
+        moveRover.move("r");
+        assertEquals(Direction.EAST, rover.getDirection());
+    }
+
+    @Test
+    public void testMultipleMovements() {
+        Rover rover = new Rover();
+        MoveRover moveRover = new MoveRover(rover);
+
+        moveRover.move("ffrff");
+        assertEquals(2, rover.getPosition().getX());
+        assertEquals(2, rover.getPosition().getY());
+        assertEquals(Direction.EAST, rover.getDirection());
+    }
 }
